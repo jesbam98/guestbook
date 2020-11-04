ed69019f5e666fad01457d907c55add6f73cd083
Jesus Bautista <jesbam98@gmail.com>
2020-11-04 17:12:21 -0600

    Add views for conference list and conference details

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,19 +12,38 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
+    private $conferenceRepository;
+
+    public function __construct(ConferenceRepository $conferenceRepository)
+    {
+        $this->conferenceRepository = $conferenceRepository;
+    }
+
     /**
      * @Route("/", name="home")
      */
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        $greet = '';
+        $conferences = $this->conferenceRepository->findAll();
 
-        if ($name = $request->query->get('hello'))
-        {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
         return $this->render('conference/index.html.twig', [
-            'greet' => $greet,
+            'conferences' => $conferences
+        ]);
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
         ]);
     }
 }
