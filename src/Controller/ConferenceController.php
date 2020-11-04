581524555f430668201d5360b70e6cb7841a8066
Jesus Bautista <jesbam98@gmail.com>
2020-11-04 14:18:10 -0600

    Add conference controller

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(Request $request): Response
+    {
+        $greet = '';
+
+        if ($name = $request->query->get('hello'))
+        {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+        return $this->render('conference/index.html.twig', [
+            'greet' => $greet,
+        ]);
+    }
+}
